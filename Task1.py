fba28c19abbe443179efd43e4136311467c1fb45
Nathan B. <nateshorty240@gmail.com>
2026-08-17 00:00:38 -0400

    Update Task1.py

--- Task1.py
+++ Task1.py
@@ -1,15 +1,12 @@
-print("Give me a name and age")
-name = input("what your name")
-age = input("whats your age")
-name_as_string = str(name)
-age_as_string = str(age)
+print("Give me a name and age") 
+name = input("what your name") 
+age = input("whats your age")  
+print(name, age)
+age_as_int = int(age) 
 
-if age > 100:
-  print("false")
-
-elif age > 0:
-  print("ahah")
-
-else:
-  print("oooo")
-
+if age_as_int > 100:
+	print("false") 
+elif age_as_int > 0: 
+	print("ahah") 
+else: 
+	print("oooo")
