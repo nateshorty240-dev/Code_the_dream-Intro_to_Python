572a5eedecd95e4e81c42db9aaa6b41c083f0394
Nathan B. <nateshorty240@gmail.com>
2026-08-16 23:37:51 -0400

    Implement name and age input with validation
    Add user input for name and age with conditional responses.

--- Task1.py
+++ Task1.py
@@ -1,1 +1,15 @@
+print("Give me a name and age")
+name = input("what your name")
+age = input("whats your age")
+name_as_string = str(name)
+age_as_string = str(age)
+
+if age > 100:
+  print("false")
+
+elif age > 0:
+  print("ahah")
+
+else:
+  print("oooo")
 
